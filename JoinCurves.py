fd082c04e6fc766e638799c69c71d9b322f153ae
tomate44 <cg@grellier.fr>
2017-03-14 14:28:08 +0100

    joinCurves now tries to increase continuity. Useful for imported SVG

--- JoinCurves.py
+++ JoinCurves.py
@@ -6,6 +6,26 @@ import dummy
 path_curvesWB = os.path.dirname(dummy.__file__)
 path_curvesWB_icons =  os.path.join( path_curvesWB, 'Resources', 'icons')
 
+DEBUG = 1
+
+def debug(string):
+    if DEBUG:
+        FreeCAD.Console.PrintMessage(string)
+        FreeCAD.Console.PrintMessage("\n")
+
+def increaseContinuity(c,tol):
+    oc = c.Continuity
+    mults = [int(m) for m in c.getMultiplicities()]
+    knots = c.getKnots()
+    for i in range(len(mults))[1:-1]:
+        c.removeKnot(i+1,mults[i]-1,tol)
+    if not oc == c.Continuity:
+        debug("Continuity increased from %s to %s"%(oc,c.Continuity))
+    else:
+        debug("Failed to increase Continuity")
+    return
+    
+
 class join:
     "joins the selected edges into a single BSpline Curve"
     def Activated(self):
@@ -31,6 +51,7 @@ class join:
                 FreeCAD.Console.PrintMessage("Failed to join edge #"+str(curves[1:].index(c)+2)+"\n")
 
         if success:
+            increaseContinuity(c0,1e-3)
             obj = FreeCAD.ActiveDocument.addObject("Part::Spline","Spline")
             obj.Shape = c0.toShape()
             for selobj in sel:
