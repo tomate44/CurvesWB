875f9df066c28c9e51b803c9078a04e08bb9ed2b
tomate44 <cg@grellier.fr>
2017-05-03 08:34:43 +0200

    WIP snap

--- bezierCurve.py
+++ bezierCurve.py
@@ -12,6 +12,8 @@ class bezierCurve:
     def Activated(self):
         self.view = FreeCADGui.ActiveDocument.ActiveView
         self.obj = FreeCAD.ActiveDocument.addObject("Part::Feature","BezierCurve")
+        #FreeCAD.ActiveDocument.recompute()
+        #self.obj.ViewObject.Selectable = False
         self.stack = [FreeCAD.Vector(0,0,0)]
         self.markerPos = None
         self.snap = False
@@ -21,8 +23,8 @@ class bezierCurve:
         self.clicCB     = self.view.addEventCallbackPivy( coin.SoMouseButtonEvent.getClassTypeId(), self.clic_cb)
         self.keyboardCB = self.view.addEventCallbackPivy( coin.SoKeyboardEvent.getClassTypeId(), self.kb_cb)
         self.cursorCB   = self.view.addEventCallbackPivy( coin.SoLocation2Event.getClassTypeId(), self.cursor_cb)
-        FreeCADGui.Selection.clearSelection()
-        FreeCADGui.Selection.addObserver(self)
+        #FreeCADGui.Selection.clearSelection()
+        #FreeCADGui.Selection.addObserver(self)
         self.nodeInit()
 
     def nodeInit(self):
@@ -46,6 +48,8 @@ class bezierCurve:
     def finish(self):
         self.curve.setPoles(self.stack[0:-1])
         self.obj.Shape = self.curve.toShape()
+        #self.obj.ViewObject.Selectable = True
+        #FreeCADGui.Selection.removeObserver(self)
         self.sg.removeChild(self.polygon)
         self.sg.removeChild(self.markers)
         self.sg.removeChild(self.coord)
@@ -53,14 +57,27 @@ class bezierCurve:
         self.view.removeEventCallbackPivy( coin.SoKeyboardEvent.getClassTypeId(), self.keyboardCB)
         self.view.removeEventCallbackPivy( coin.SoMouseButtonEvent.getClassTypeId(), self.clicCB)
 
-    def getSnapPoint(self, pt):
-        return(pt)
+    def getSnapPoint(self):
+        v = Part.Vertex(self.point)
+        dist, pts, sols = v.distToShape(self.snapShape)
+        self.point = pts[0][1]
 
-    def addPreSelection(self,doc,obj,sub):
-        self.snapShape = self.getShape(doc,obj,sub)
-
-    def removePreSelection(self,doc,obj,sub):
-        self.snapShape = None
+    def getSnapShape(self,info):
+        if not info == None:
+            try:
+                doc, obj, sub = info['Document'], info['Object'], info['Component']
+            except KeyError, TypeError:
+                self.snapShape = None
+                return(False)
+        snapobj = FreeCAD.getDocument(doc).getObject(obj)
+        if 'Edge' in sub:
+            n = eval(sub.lstrip('Edge'))
+            self.snapShape = snapobj.Shape.Edges[n-1]
+            return(True)
+        elif 'Face' in sub:
+            n = eval(sub.lstrip('Face'))
+            self.snapShape = snapobj.Shape.Faces[n-1]
+            return(True)
 
     def kb_cb(self, event_callback):
         event = event_callback.getEvent()
@@ -91,8 +108,10 @@ class bezierCurve:
         event = event_callback.getEvent()
         pos = event.getPosition()
         self.point = self.view.getPoint(pos[0],pos[1])
-        if self.snap and not (self.snapShape == None):
-            self.point = self.getSnapPoint(point)
+        if self.snap:
+            listObjects = FreeCADGui.ActiveDocument.ActiveView.getObjectsInfo((int(pos[0]),int(pos[1])))
+            if self.getSnapShape(listObjects):
+                self.getSnapPoint()
         l = len(self.coord.point.getValues())
         self.coord.point.set1Value(l-1,self.point)
         self.stack[-1] = self.point
@@ -104,3 +123,4 @@ class bezierCurve:
     def GetResources(self):
         return {'Pixmap' : path_curvesWB_icons+'/bezier.svg', 'MenuText': 'Bezier Curve', 'ToolTip': 'Creates a Bezier curve by clicking 4 points on the screen'}
 FreeCADGui.addCommand('bezierCurve', bezierCurve())
+ 
