d173972e33870d34d5f9427179c30f92dff322f6
tomate44 <cg@grellier.fr>
2018-04-27 13:42:45 +0200

    EasyProxy class

--- _utils.py
+++ _utils.py
@@ -49,3 +49,108 @@ def getShape(obj, prop, shape_type):
     else:
         FreeCAD.Console.PrintError("CurvesWB._utils.getShape: %r has no property %r\n"%(obj, prop))
         return(None)
+
+
+
+class EasyProxy(object):
+    def __init__(self, fp):
+        self.document_restored = True
+        self.ep_add_properties(fp)
+        fp.Proxy = self
+        self.ep_init(fp)
+
+    def execute(self, fp):
+        if not self.document_restored:
+            debug("Skipping %s.execute() ..."%fp.Label)
+            return(False)
+        else:
+            self.ep_execute(fp)
+
+    def onChanged(self, fp, prop):
+        if not self.document_restored:
+            debug("Skipping %s.onChanged(%s) ..."%(fp.Label,prop))
+            return(False)
+        else:
+            self.ep_prop_changed(fp, prop)
+
+    def onBeforeChange(self, fp, prop):
+        if prop == "Proxy":
+            return(False)
+        if not self.document_restored:
+            debug("Skipping %s.onBeforeChange(%s) ..."%(fp.Label,prop))
+            return(False)
+        else:
+            self.ep_before_prop_change(fp, prop)
+
+    def onDocumentRestored(self, fp):
+        self.document_restored = True
+        debug("%s restored !"%fp.Label)
+        self.ep_init(fp)
+
+    def __getstate__(self):
+        debug("EasyProxy.__getstate__")
+        state = self.ep_on_save()
+        # add additional instance variables
+        # state["variable"] = self.variable
+        return(state)
+
+    def __setstate__(self,state):
+        debug("EasyProxy.__setstate__")
+        self.document_restored = False
+        self.ep_on_restore(state)
+        # restore additional instance variables
+        # self.variable = state["variable"]
+        return(None)
+
+    def ep_add_properties(self, fp):
+        #fp.addProperty("App::PropertyInteger", "myprop", "Test", "a property").myprop = 1
+        return(None)
+
+    def ep_init(self, fp):
+        return(None)
+
+    def ep_execute(self, fp):
+        return(None)
+
+    def ep_prop_changed(self, fp, prop):
+        return(None)
+
+    def ep_before_prop_change(self, fp, prop):
+        return(None)
+
+    def ep_on_save(self):
+        return(dict())
+
+    def ep_on_restore(self, state):
+        return(None)
+
+
+
+class MyProxy(EasyProxy):
+
+    def ep_add_properties(self, fp):
+        debug("---MyProxy.ep_add_properties")
+        fp.addProperty("App::PropertyInteger", "mprop", "Test", "a property").mprop = 1
+        fp.addProperty("App::PropertyInteger", "aprop", "Test", "a property").aprop = 2
+        fp.addProperty("App::PropertyInteger", "zprop", "Test", "a property").zprop = 3
+
+    def ep_init(self, fp):
+        debug("---MyProxy.ep_init(%s)"%fp.Label)
+
+    def ep_execute(self, fp):
+        debug("---MyProxy.ep_execute(%s)"%fp.Label)
+
+    def ep_prop_changed(self, fp, prop):
+        debug("---MyProxy.ep_prop_changed: %s(%s)"%(fp.Label,prop))
+
+    def ep_before_prop_change(self, fp, prop):
+        debug("---MyProxy.ep_before_prop_change: %s(%s)"%(fp.Label,prop))
+
+    def ep_on_save(self):
+        debug("---MyProxy.ep_on_save")
+        return(None)
+
+    def ep_on_restore(self,state):
+        debug("---MyProxy.ep_on_restore")
+        return(None)
+
