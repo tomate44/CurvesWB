fada66861a1de0d7e80b9700dc3fe297e2afe3a0
tomate44 <cg@grellier.fr>
2018-08-20 18:51:22 +0200

    sublink editor WIP

--- sublink_edit.py
+++ sublink_edit.py
@@ -38,14 +38,13 @@ class proxyVP(object):
         debug("VP init")
         obj.Proxy = self
 
+    def attach(self, vobj):
+        self.Object = vobj.Object
+
     def setEdit(self,vobj,mode):
         debug("Start Edit / mode: %d"%mode)
         # https://www.freecadweb.org/wiki/PySide_Advanced_Examples/fr
-        main_win = FreeCADGui.getMainWindow()
-        myWidget = QtGui.QDockWidget()
-        myWidget.ui = myWidget_Ui()
-        myWidget.ui.setupUi(myWidget)
-        main_win.addDockWidget(QtCore.Qt.RightDockWidgetArea,myWidget) 
+        self.edit_widget = SubLinkEditorWidget(self.Object)
         return(True)
 
     def unsetEdit(self,vobj,mode):
@@ -53,81 +52,81 @@ class proxyVP(object):
         #self.combo.removeTab(self.tab.idx) 
         return(True)
 
+class SubLinkEditorWidget(object):
+    def __init__(self, obj):
+        self.main_win = FreeCADGui.getMainWindow()
+        self.widget = QtGui.QDockWidget()
+        self.widget.ui = myWidget_Ui(obj)
+        self.widget.ui.setupUi(self.widget)
+        self.main_win.addDockWidget(QtCore.Qt.LeftDockWidgetArea,self.widget) 
+
 class myWidget_Ui(object):
-  def setupUi(self, myWidget):
-    myWidget.setObjectName("Sublinks Editor")
-    myWidget.resize(QtCore.QSize(300,100).expandedTo(myWidget.minimumSizeHint())) # sets size of the widget
-
-    self.label = QtGui.QLabel(myWidget) # creates a label
-    self.label.setGeometry(QtCore.QRect(50,50,200,24)) # sets its size
-    self.label.setObjectName("Editor") # sets its name, so it can be found by name
-
-  def retranslateUi(self, draftToolbar): # built-in QT function that manages translations of widgets
-    myWidget.setWindowTitle(QtGui.QApplication.translate("myWidget", "Sublinks Editor", None, QtGui.QApplication.UnicodeUTF8))
-    self.label.setText(QtGui.QApplication.translate("myWidget", "Sublinks Editor", None, QtGui.QApplication.UnicodeUTF8)) 
-
-class editGui(QtGui.QWidget):  
-    def __init__(self):
-        super(editGui, self).__init__()
-        self.init_ui()
-        self.parent = None
-        self.idx = None
-
-    def init_ui(self):
-        link_sub = list()
-        link_sub_list = list()
-        sel = FreeCADGui.Selection.getSelection()
-        if sel == []:
-            self.cancel()
-            return(False)
-        else:
-            sel = sel[-1]
-        for pro in sel.PropertiesList:
-            if sel.getTypeIdOfProperty(pro) == 'App::PropertyLinkSub':
+    def __init__(self, obj):
+        self.link_sub = list()
+        self.link_sub_list = list()
+        #sel = FreeCADGui.Selection.getSelection()
+        #if sel == []:
+            #self.cancel()
+            #return(False)
+        #else:
+            #sel = sel[-1]
+        for pro in obj.PropertiesList:
+            if obj.getTypeIdOfProperty(pro) == 'App::PropertyLinkSub':
                 print("PropertyLinkSub -> %s"%pro)
-                link_sub.append(pro)
-            elif sel.getTypeIdOfProperty(pro) == 'App::PropertyLinkSubList':
+                self.link_sub.append(pro)
+            elif obj.getTypeIdOfProperty(pro) == 'App::PropertyLinkSubList':
                 print("PropertyLinkSubList -> %s"%pro)
-                link_sub_list.append(pro)
+                self.link_sub_list.append(pro)
+    def setupUi(self, myWidget):
+        myWidget.setObjectName("Sublinks Editor")
+        #myWidget.resize(QtCore.QSize(300,100).expandedTo(myWidget.minimumSizeHint())) # sets size of the widget
 
-        vbox_1 = QtGui.QVBoxLayout()
-        label_1 = QtGui.QLabel(self)
-        label_1.setText('LinkSub')
-        vbox_1.addWidget(label_1,1)
+#        self.label = QtGui.QLabel(myWidget) # creates a label
+#        self.label.setGeometry(QtCore.QRect(50,50,200,24)) # sets its size
+#        self.label.setObjectName("Editor") # sets its name, so it can be found by name
 
-        for link in link_sub:
-            label_link = QtGui.QLabel(self)
+        self.vbox_1 = QtGui.QVBoxLayout()
+        self.label_1 = QtGui.QLabel(myWidget)
+        self.label_1.setText('LinkSub')
+        self.vbox_1.addWidget(self.label_1, 1, QtCore.Qt.AlignCenter)
+
+        for link in self.link_sub:
+            label_link = QtGui.QLabel(myWidget)
             label_link.setText(link)
             set_button = QtGui.QPushButton('Select')
             set_button.clicked.connect(self.select)
             done_button = QtGui.QPushButton('Done')
             done_button.clicked.connect(self.endselect)
             hbox = QtGui.QHBoxLayout()
-            vbox_1.addWidget(label_link, 1, QtCore.Qt.AlignCenter)
-            hbox.addWidget(set_button, 1)
-            hbox.addWidget(done_button, 1)
-            vbox_1.addLayout(hbox)
+            self.vbox_1.addWidget(label_link, 1, QtCore.Qt.AlignCenter)
+            hbox.addWidget(set_button, 1, QtCore.Qt.AlignCenter)
+            hbox.addWidget(done_button, 1, QtCore.Qt.AlignCenter)
+            self.vbox_1.addLayout(hbox)
 
         ok_button = QtGui.QPushButton('OK')
-        ok_button.clicked.connect(self.close)
+        ok_button.clicked.connect(self.ok)
 
         cancel_button = QtGui.QPushButton('Cancel')
         cancel_button.clicked.connect(self.cancel)
 
         hbox = QtGui.QHBoxLayout()
-        hbox.addWidget(ok_button, 1)
-        hbox.addWidget(cancel_button, 1)
-        vbox_1.addStretch(1)
-        vbox_1.addLayout(hbox)
+        hbox.addWidget(ok_button, 1, QtCore.Qt.AlignCenter)
+        hbox.addWidget(cancel_button, 1, QtCore.Qt.AlignCenter)
+        self.vbox_1.addStretch(1)
+        self.vbox_1.addLayout(hbox)
 
-        self.setLayout(vbox_1)
+        myWidget.setLayout(self.vbox_1)
 
         desktop_widget = QtGui.QDesktopWidget()
         right = desktop_widget.availableGeometry().width()
 
-        #self.setGeometry(right - 300, 0, 300, 150)
-        self.setWindowTitle('Edit linkSub property')
-        self.show()
+        myWidget.setGeometry(right - 300, 0, 300, 150)
+        myWidget.setWindowTitle('Edit linkSub property')
+        #myWidget.show()
+
+    def retranslateUi(self, draftToolbar): # built-in QT function that manages translations of widgets
+        myWidget.setWindowTitle(QtGui.QApplication.translate("myWidget", "Sublinks Editor", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("myWidget", "Sublinks Editor", None, QtGui.QApplication.UnicodeUTF8)) 
 
     def select(self):
         return()
@@ -142,8 +141,8 @@ class editGui(QtGui.QWidget):
         self.quit()
     
     def quit(self):
-        #self.close()
-        self.parent.removeTab(self.idx)
+        pass
+
 
 
 doc = FreeCAD.ActiveDocument
