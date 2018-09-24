a86b95d58dc59c53aa4a934f01ad9d25146fef7c
tomate44 <cg@grellier.fr>
2018-09-24 08:49:51 +0200

    make ExtractShapes work with objects that have a global placement

--- ExtractShapes.py
+++ ExtractShapes.py
@@ -21,6 +21,9 @@ class extract:
             for so,name in zip(o.SubObjects,o.SubElementNames):
                 fullname = objName+"_"+name
                 newobj = FreeCAD.ActiveDocument.addObject("Part::Feature",fullname)
+                if hasattr(o.Object, "getGlobalPlacement"):
+                    gpl = o.Object.getGlobalPlacement()
+                    so.transformShape(gpl.toMatrix())
                 newobj.Shape = so
             o.Object.ViewObject.Visibility = False
         FreeCAD.ActiveDocument.recompute()
