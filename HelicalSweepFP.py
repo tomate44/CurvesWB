af6743f4321d1abb07679267175f29f929312418
tomate44 <cg@grellier.fr>
2020-03-29 19:46:04 +0200

    HelicalSweep: fix solid construction

--- HelicalSweepFP.py
+++ HelicalSweepFP.py
@@ -81,76 +81,92 @@ class HelicalSweep:
     def __init__(self):
         self._plane = Part.Plane()
         self.nb_of_turns = 2.5
-        self.lead = 20.0
-        self.flatends = False
+        self.lead = 10.0
         self.cylinder = Part.Cylinder()
         self.cylinder.transform(FreeCAD.Placement(FreeCAD.Vector(),FreeCAD.Vector(1,0,0),-90).toMatrix())
         self.rational_approx = True
         self.check_splines = False
-    def set_placement(self,pl):
+        self.max_radius = 0
+        self._placement = FreeCAD.Placement()
+    def set_placement(self, pl):
+        self._placement = pl
         self._plane.transform(pl.toMatrix())
         self.cylinder.transform(pl.toMatrix())
-    def sweep_Point2D(self, v, y=0):
-        if isinstance(v, FreeCAD.Base.Vector2d):
-            sp = vec2(0,v.y)
-            self.cylinder.Radius = v.x
-        else:
-            sp = vec2(0,y)
-            self.cylinder.Radius = v
+    def sweep_Point2D(self, v, extend=False):
+        offset = 0
+        nb_turns = self.nb_of_turns
+        if extend:
+            offset = -self.lead*2
+            nb_turns = self.nb_of_turns + 4
+        sp = vec2(0, v.y + offset)
+        self.cylinder.Radius = v.x
+        if v.x > self.max_radius:
+            self.max_radius = v.x
         vector_one_turn = vec2(2*pi,self.lead)
-        vector_full_turns = vmul(vector_one_turn, self.nb_of_turns)
+        vector_full_turns = vmul(vector_one_turn, nb_turns)
         lineseg = Part.Geom2d.Line2dSegment(sp, vadd(sp,vector_full_turns))
-        if self.flatends:
-            line = Part.Geom2d.Line2d(sp, vadd(sp,vector_full_turns))
-            horiz = Part.Geom2d.Line2d(vec2(0,0), vec2(1,0))
-            inter = line.intersectCC(horiz)[0]
-            lineseg = Part.Geom2d.Line2dSegment(inter, vadd(inter,vector_full_turns))
         return lineseg.toShape(self.cylinder)
-    def sweep_edge(self, e):
-        #if isinstance(e.Curve,Part.Line):
-            #fp = e.valueAt(e.FirstParameter)
-            #lp = e.valueAt(e.LastParameter)
-            #fp2d = vec2(*self._plane.projectPoint(fp,"LowerDistanceParameters"))
-            #lp2d = vec2(*self._plane.projectPoint(lp,"LowerDistanceParameters"))
-            #e1 = self.sweep_Point2D(fp2d)
-            #e2 = self.sweep_Point2D(lp2d)
-            #r = Part.makeRuledSurface(e1,e2)
-            #return r
-        if True:
-            if self.rational_approx:
-                approx = e.toNurbs().Edges[0]
-            else:
-                approx = e.Curve.toBSpline(e.FirstParameter,e.LastParameter).toShape()
-            bs = approx.Curve
-            #self.cylinder.transform(self.placement.toMatrix())
-            poles = []
-            weights = []
-            oc = False
-            for w,p in zip(bs.getWeights(), bs.getPoles()):
-                p2d = vec2(*self._plane.projectPoint(p,"LowerDistanceParameters"))
-                c = self.sweep_Point2D(p2d).Curve
-                if oc and self.check_splines:
-                    nurbs_tools.is_same(oc, c, 1e-3, True)
-                oc = c
-                poles.append(oc.getPoles())
-                weights.append([w]*oc.NbPoles)
-            bss = Part.BSplineSurface()
-            bss.buildFromPolesMultsKnots(poles,
-                                         bs.getMultiplicities(),oc.getMultiplicities(),  
-                                         bs.getKnots(), oc.getKnots(),
-                                         bs.isPeriodic(), oc.isPeriodic(),
-                                         bs.Degree, oc.Degree,
-                                         weights)
-            return bss.toShape()
-                
-    def sweep_wire(self, w):
+    def sweep_edge(self, e, extend=False):
+        if self.rational_approx:
+            approx = e.toNurbs().Edges[0]
+        else:
+            approx = e.Curve.toBSpline(e.FirstParameter,e.LastParameter).toShape()
+        bs = approx.Curve
+        #self.cylinder.transform(self.placement.toMatrix())
+        poles = []
+        weights = []
+        oc = False
+        for w,p in zip(bs.getWeights(), bs.getPoles()):
+            p2d = vec2(*self._plane.projectPoint(p,"LowerDistanceParameters"))
+            c = self.sweep_Point2D(p2d, extend).Curve
+            if oc and self.check_splines:
+                nurbs_tools.is_same(oc, c, 1e-3, True)
+            oc = c
+            poles.append(oc.getPoles())
+            weights.append([w]*oc.NbPoles)
+        bss = Part.BSplineSurface()
+        bss.buildFromPolesMultsKnots(poles,
+                                        bs.getMultiplicities(),oc.getMultiplicities(),  
+                                        bs.getKnots(), oc.getKnots(),
+                                        bs.isPeriodic(), oc.isPeriodic(),
+                                        bs.Degree, oc.Degree,
+                                        weights)
+        return bss.toShape()
+    def sweep_wire(self, w, solid=False):
         faces = []
         for e in w.Edges:
-            faces.append(self.sweep_edge(e))
-        try:
-            return Part.Shell(faces)
-        except Part.OCCError:
-            return Part.Compound(faces)
+            faces.append(self.sweep_edge(e,solid))
+        shell = Part.Shell(faces)
+        shell.sewShape()
+        if solid:
+            cyl = Part.makeCylinder(self.max_radius*2, self.nb_of_turns*self.lead)
+            cyl.Placement = self._placement.multiply(FreeCAD.Placement(FreeCAD.Vector(),FreeCAD.Vector(1,0,0),-90))
+            common = cyl.common(shell)
+            cut_faces = common.Faces
+            new_edges = []
+            for e1 in common.Edges:
+                found = False
+                for e2 in shell.Edges:
+                    if nurbs_tools.is_same(e1.Curve, e2.Curve, tol=1e-7, full=False):
+                        found = True
+                        #print("found similar edges")
+                        continue
+                if not found:
+                    new_edges.append(e1)
+            #print(len(Part.sortEdges(new_edges)))
+            el1, el2 = Part.sortEdges(new_edges)[0:2]
+            f1 = Part.makeFace(Part.Wire(el1),'Part::FaceMakerSimple')
+            f2 = Part.makeFace(Part.Wire(el2),'Part::FaceMakerSimple')
+            cut_faces.extend([f1,f2])
+            try:
+                shell = Part.Shell(cut_faces)
+                shell.sewShape()
+                return Part.Solid(shell)
+            except Part.OCCError:
+                print("Failed to create solid")
+                return Part.Compound(cut_faces)
+        return shell
+
 
 
 class HelicalSweepFP:
@@ -161,6 +177,7 @@ class HelicalSweepFP:
         obj.addProperty("App::PropertyFloat", "Turns", "Settings", "Number of turns").Turns = 1.0
         obj.addProperty("App::PropertyFloat", "Lead", "Settings", "Thread lead (-1 for auto)").Lead = -1
         obj.addProperty("App::PropertyBool", "Rational", "Settings", "Allow rational bsplines").Rational = False
+        obj.addProperty("App::PropertyBool", "Solid", "Settings", "Create a solid shape").Solid = False
         obj.Proxy = self
 
     def execute(self, obj):
@@ -168,18 +185,22 @@ class HelicalSweepFP:
         hs = HelicalSweep()
         hs.rational_approx = obj.Rational
         gpl = obj.Profile.getGlobalPlacement()
-        #o = gpl.multVec(FreeCAD.Vector(0,0,0))
-        #x = gpl.multVec(FreeCAD.Vector(1,0,0))
-        #y = gpl.multVec(FreeCAD.Vector(0,1,0))
-        #hs._plane.transform(gpl.toMatrix())
         hs.set_placement(gpl)
         if hasattr(obj,"Lead") and obj.Lead >= 0:
             hs.lead = obj.Lead
         else:
-            hs.lead = obj.Profile.getDatum("Lead").Value
+            try:
+                hs.lead = obj.Profile.getDatum("Lead").Value
+            except NameError:
+                print("Profile sketch has no 'Lead' constraint")
+                hs.lead = 10.0
         hs.nb_of_turns = obj.Turns
-        faces = [hs.sweep_edge(e) for e in edges]
-        obj.Shape = Part.Compound(faces)
+        obj.Shape = hs.sweep_wire(Part.Wire(edges), obj.Solid)
+        
+        #faces = [hs.sweep_edge(e, obj.Solid) for e in edges]
+        #shell = Part.Shell(faces)
+        #shell.sewShape()
+        #obj.Shape = shell
 
     def onChanged(self, obj, prop):
         if prop == "Lead":
