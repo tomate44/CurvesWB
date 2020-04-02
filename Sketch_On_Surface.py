012a2d866aa4cf3a9670fd39958c6c1f755ace30
tomate44 <cg@grellier.fr>
2020-04-02 18:48:40 +0200

    Sketch_on_Surface: improve facemaker

--- Sketch_On_Surface.py
+++ Sketch_On_Surface.py
@@ -139,6 +139,56 @@ def stretched_plane(geom_range=[0,1,0,1], param_range=[0,2,0,2]):
     bs.buildFromPolesMultsKnots(poles, umults, vmults, uknots, vknots, False, False, 1, 1)
     return bs
 
+class BoundarySorter:
+    def __init__(self, wires):
+        self.wires = []
+        self.parents = []
+        self.sorted_wires = []
+        for w in wires:
+            self.wires.append(w)
+            self.parents.append([])
+            self.sorted_wires.append([])
+        self.done = False
+    def check_inside(self):
+        for i, w1 in enumerate(self.wires):
+            for j, w2 in enumerate(self.wires):
+                if not i == j:
+                    if w2.BoundBox.isInside(w1.BoundBox):
+                        self.parents[i].append(j)
+    def sort_pass(self):
+        to_remove = []
+        for i,p in enumerate(self.parents):
+            if (p is not None) and p == []:
+                to_remove.append(i)
+                self.sorted_wires[i].append(self.wires[i])
+                self.parents[i] = None
+        #print("Removing parents : {}".format(to_remove))
+        for i,p in enumerate(self.parents):
+            if (p is not None) and len(p) == 1:
+                to_remove.append(i)
+                self.sorted_wires[p[0]].append(self.wires[i])
+                self.parents[i] = None
+        #print("Removing full : {}".format(to_remove))
+        if len(to_remove) > 0:
+            for i,p in enumerate(self.parents):
+                if (p is not None):
+                    for r in to_remove:
+                        if r in p:
+                            p.remove(r)
+        else:
+            self.done = True
+    def sort(self):
+        self.check_inside()
+        #print(self.parents)
+        while not self.done:
+            #print("Pass {}".format(i))
+            self.sort_pass()
+        result = []
+        for w in self.sorted_wires:
+            if w:
+                result.append(w)
+        return result
+
 class sketchOnSurface:
     "This feature object maps a sketch on a surface"
     def __init__(self, obj):
@@ -151,22 +201,15 @@ class sketchOnSurface:
         obj.Proxy = self
 
     def build_faces(self, wl, surf):
-        outer = wl[:]
-        inner = []
-        for i in range(len(wl)):
-            for j in range(len(wl)):
-                if (not i == j) and wl[i].BoundBox.isInside(wl[j].BoundBox):
-                    #print("Wire {} is a hole".format(i))
-                    outer.remove(wl[i])
-                    inner.append(wl[i])
         faces = []
-        for w1 in outer:
-            f = Part.Face(surf, w1)
+        bs = BoundarySorter(wl)
+        for wirelist in bs.sort():
+            #print(wirelist)
+            f = Part.Face(surf, wirelist[0])
             f.validate()
-            for w2 in inner:
-                if w2.BoundBox.isInside(f.BoundBox):
-                    f.cutHoles([w2])
-                    f.validate()
+            if len(wirelist) > 1:
+                f.cutHoles(wirelist[1:])
+                f.validate()
             faces.append(f)
         return faces
 
