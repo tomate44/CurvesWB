0dc741042324aed225a1461945f0dbb4622a6fec
tomate44 <cg@grellier.fr>
2020-11-17 18:47:03 +0100

    fix double selection

--- freecad/Curves/approximate.py
+++ freecad/Curves/approximate.py
@@ -53,6 +53,7 @@ debug = _utils.doNothing
 
 DEBUG = 0
 
+
 def debug(string):
     if DEBUG:
         FreeCAD.Console.PrintMessage(string)
@@ -60,7 +61,7 @@ def debug(string):
 
 
 class Approximate:
-    def __init__(self, obj , source):
+    def __init__(self, obj, source):
         ''' Add the properties '''
         debug("\nApproximate class Init\n")
         obj.addProperty("App::PropertyLink",           "PointObject",    "Approximate","Object containing the points to approximate").PointObject = source
@@ -81,58 +82,58 @@ class Approximate:
         obj.Proxy = self
         self.obj = obj
         self.Points = []
-        obj.LengthWeight =    (1.0,0.01,10.0,0.1)
-        obj.CurvatureWeight = (1.0,0.01,10.0,0.1)
-        obj.TorsionWeight =   (1.0,0.01,10.0,0.1)
+        obj.LengthWeight = (1.0, 0.01, 10.0, 0.1)
+        obj.CurvatureWeight = (1.0, 0.01, 10.0, 0.1)
+        obj.TorsionWeight = (1.0, 0.01, 10.0, 0.1)
         obj.Method = "Parametrization"
         obj.Parametrization = "ChordLength"
         obj.Continuity = 'C2'
         self.getPoints(obj)
         self.setTolerance(obj)
-        #obj.ApproxTolerance = 0.05
+        # obj.ApproxTolerance = 0.05
         obj.LastIndex = len(self.Points)-1
-        #self.execute(obj)
+        # self.execute(obj)
 
     def setTolerance(self, obj):
         try:
-            l = obj.PointObject.Shape.BoundBox.DiagonalLength
-            obj.ApproxTolerance = l / 10000.0
+            le = obj.PointObject.Shape.BoundBox.DiagonalLength
+            obj.ApproxTolerance = le / 10000.0
         except:
             obj.ApproxTolerance = 0.001
 
-    def getPoints( self, obj):
-        if hasattr(obj.PointObject,'Group'):
+    def getPoints(self, obj):
+        if hasattr(obj.PointObject, 'Group'):
             a = []
             for o in obj.PointObject.Group:
-                if hasattr(o,'Points'):
+                if hasattr(o, 'Points'):
                     a.append(o.Points)
                 else:
                     a.append([v.Point for v in o.Shape.Vertexes])
             self.Points = a
-        elif hasattr(obj.PointObject,'ProfileSamples'):
-            numVert = len(obj.PointObject.Shape.Vertexes) 
+        elif hasattr(obj.PointObject, 'ProfileSamples'):
+            numVert = len(obj.PointObject.Shape.Vertexes)
             debug("Surface object detected")
-            debug("%d points"%numVert)
+            debug("%d points" % numVert)
             n = numVert / obj.PointObject.ProfileSamples
             a = []
             r = []
             for i in range(numVert):
                 r.append(obj.PointObject.Shape.Vertexes[i].Point)
-                if (not i == 0) and ((i+1)%obj.PointObject.ProfileSamples == 0):
+                if (not i == 0) and ((i+1) % obj.PointObject.ProfileSamples == 0):
                     a.append(r)
                     r = []
-            #a.append(r)
+            # a.append(r)
             if not a == []:
                 self.Points = a
-                debug("Array : %d x %d"%(len(a),len(a[0])))
+                debug("Array : %d x %d" % (len(a), len(a[0])))
             else:
                 self.Points = []
-        elif hasattr(obj.PointObject,'Points'):
+        elif hasattr(obj.PointObject, 'Points'):
             if isinstance(obj.PointObject.Points, (list, tuple)):
                 self.Points = obj.PointObject.Points
-            elif hasattr(obj.PointObject.Points,'Points'):
+            elif hasattr(obj.PointObject.Points, 'Points'):
                 self.Points = obj.PointObject.Points.Points
-        elif hasattr(obj.PointObject.Shape,'OrderedVertexes'):
+        elif hasattr(obj.PointObject.Shape, 'OrderedVertexes'):
             self.Points = [v.Point for v in obj.PointObject.Shape.OrderedVertexes]
         else:
             self.Points = [v.Point for v in obj.PointObject.Shape.Vertexes]
@@ -140,7 +141,7 @@ class Approximate:
     def buildCurve(self, obj):
         pts = self.Points[obj.FirstIndex:obj.LastIndex+1]
         bs = Part.BSplineCurve()
-        if (obj.Method == "Parametrization") and (obj.Parametrization == "Curvilinear") and (hasattr(obj.PointObject,"Distance")):
+        if (obj.Method == "Parametrization") and (obj.Parametrization == "Curvilinear") and (hasattr(obj.PointObject, "Distance")):
             params = []
             try:
                 dis = obj.PointObject.Distance
@@ -150,7 +151,7 @@ class Approximate:
                 params.append(1.0 * i * dis)
             lv = pts[-1].sub(pts[-2])
             params[-1] = params[-2] + lv.Length
-            bs.interpolate(Points = pts, Parameters = params, Tolerance = obj.ApproxTolerance)
+            bs.interpolate(Points=pts, Parameters=params, Tolerance=obj.ApproxTolerance)
 
         elif obj.Method == "Parametrization":
             bs.approximate(Points = pts, DegMin = obj.DegreeMin, DegMax = obj.DegreeMax, Tolerance = obj.ApproxTolerance, Continuity = obj.Continuity, ParamType = obj.Parametrization)
@@ -178,40 +179,40 @@ class Approximate:
     def execute(self, obj):
         debug("\n* Approximate : execute *\n")
         num = len(self.Points)
-        diff = num - obj.LastIndex -1
-        self.getPoints( obj)
-        #obj.FirstIndex = 0
+        diff = num - obj.LastIndex - 1
+        self.getPoints(obj)
+        # obj.FirstIndex = 0
         obj.LastIndex = len(self.Points)-diff-1
-        if isinstance(self.Points[0],list):
-            self.buildSurf( obj)
+        if isinstance(self.Points[0], list):
+            self.buildSurf(obj)
         else:
-            self.buildCurve( obj)
+            self.buildCurve(obj)
         obj.Shape = self.curve.toShape()
 
     def onChanged(self, fp, prop):
-        #print fp
+        # print fp
         if not fp.PointObject:
             return
         if prop == "PointObject":
             debug("Approximate : PointObject changed\n")
             num = len(self.Points)
-            diff = num - fp.LastIndex -1
-            self.getPoints( fp)
-            #fp.FirstIndex = 0
+            diff = num - fp.LastIndex - 1
+            self.getPoints(fp)
+            # fp.FirstIndex = 0
             fp.LastIndex = len(self.Points)-diff
 
         if prop == "Parametrization":
             debug("Approximate : Parametrization changed\n")
-            props = ["ClampEnds","DegreeMin","DegreeMax","Continuity"]
+            props = ["ClampEnds", "DegreeMin", "DegreeMax", "Continuity"]
             if fp.Parametrization == "Curvilinear":
-                if hasattr(fp.PointObject,"Distance"):
+                if hasattr(fp.PointObject, "Distance"):
                     for p in props:
                         fp.setEditorMode(p, 2)
                 else:
                     fp.Parametrization == "ChordLength"
             else:
                 for p in props:
-                    fp.setEditorMode(p, 0)   
+                    fp.setEditorMode(p, 0)
 
         if prop == "Method":
             debug("Approximate : Method changed\n")
@@ -225,15 +226,15 @@ class Approximate:
                 fp.setEditorMode("LengthWeight", 0)
                 fp.setEditorMode("CurvatureWeight", 0)
                 fp.setEditorMode("TorsionWeight", 0)
-                if fp.Continuity in ["C3","CN"]:
+                if fp.Continuity in ["C3", "CN"]:
                     fp.Continuity = 'C2'
-                    
+
         if prop == "Continuity":
             if fp.Method == "Smoothing Algorithm":
                 if fp.Continuity == 'C1':
                     if fp.DegreeMax < 3:
                         fp.DegreeMax = 3
-                elif fp.Continuity in ['G1','G2','C2']:
+                elif fp.Continuity in ['G1', 'G2', 'C2']:
                     if fp.DegreeMax < 5:
                         fp.DegreeMax = 5
             debug("Approximate : Continuity changed to "+str(fp.Continuity))
@@ -250,19 +251,19 @@ class Approximate:
             elif fp.DegreeMax > 14:
                 fp.DegreeMax = 14
             if fp.Method == "Smoothing Algorithm":
-                if fp.Continuity in ['G1','G2','C2']:
+                if fp.Continuity in ['G1', 'G2', 'C2']:
                     if fp.DegreeMax < 5:
                         fp.DegreeMax = 5
                 elif fp.Continuity == "C1":
                     if fp.DegreeMax < 3:
                         fp.DegreeMax = 3
-            debug("Approximate : DegreeMax changed to "+str(fp.DegreeMax))
+            debug("Approximate : DegreeMax changed to " + str(fp.DegreeMax))
         if prop == "ApproxTolerance":
             if fp.ApproxTolerance < 1e-6:
                 fp.ApproxTolerance = 1e-6
             elif fp.ApproxTolerance > 1000.0:
                 fp.ApproxTolerance = 1000.0
-            debug("Approximate : ApproxTolerance changed to "+str(fp.ApproxTolerance))
+            debug("Approximate : ApproxTolerance changed to " + str(fp.ApproxTolerance))
 
         if prop == "FirstIndex":
             if fp.FirstIndex < 0:
@@ -277,66 +278,71 @@ class Approximate:
                 fp.LastIndex = len(self.Points)-1
             debug("Approximate : LastIndex changed to "+str(fp.LastIndex))
 
-            
     def __getstate__(self):
         out = {"name": self.obj.Name,
                "Method": self.obj.Method}
         return out
 
-    def __setstate__(self,state):
+    def __setstate__(self, state):
         self.obj = FreeCAD.ActiveDocument.getObject(state["name"])
-        if not "Method" in self.obj.PropertiesList:
-            self.obj.addProperty("App::PropertyEnumeration",  "Method",       "General",     "Approximation method").Method=["Parametrization","Smoothing Algorithm"]
-        if not "TorsionWeight" in self.obj.PropertiesList:
-            self.obj.addProperty("App::PropertyFloatConstraint",        "TorsionWeight",   "Parameters",       "Weight of curve torsion for smoothing algorithm")
+        if "Method" not in self.obj.PropertiesList:
+            self.obj.addProperty("App::PropertyEnumeration",
+                                 "Method",
+                                 "General",
+                                 "Approximation method").Method = ["Parametrization", "Smoothing Algorithm"]
+        if "TorsionWeight" not in self.obj.PropertiesList:
+            self.obj.addProperty("App::PropertyFloatConstraint",
+                                 "TorsionWeight",
+                                 "Parameters",
+                                 "Weight of curve torsion for smoothing algorithm")
         self.obj.Method = state["Method"]
         self.getPoints(self.obj)
         return None
 
+
 class ViewProviderApp:
-    def __init__(self,vobj):
+    def __init__(self, vobj):
         vobj.Proxy = self
-       
+
     def getIcon(self):
         return (TOOL_ICON)
 
     def attach(self, vobj):
         self.Object = vobj.Object
-  
-    def setEdit(self,vobj,mode):
+
+    def setEdit(self, vobj, mode):
         return False
-    
-    def unsetEdit(self,vobj,mode):
+
+    def unsetEdit(self, vobj, mode):
         return
 
     def __getstate__(self):
         return {"name": self.Object.Name}
 
-    def __setstate__(self,state):
+    def __setstate__(self, state):
         self.Object = FreeCAD.ActiveDocument.getObject(state["name"])
         return None
 
     def claimChildren(self):
         return [self.Object.PointObject]
-        
+
     def onDelete(self, feature, subelements):
         try:
-            self.Object.PointObject.ViewObject.Visibility=True
+            self.Object.PointObject.ViewObject.Visibility = True
         except Exception as err:
             FreeCAD.Console.PrintError("Error in onDelete: {0} \n".format(err))
         return True
-
 
 
 class approx:
     def parseSel(self, selectionObject):
         res = []
         for obj in selectionObject:
-            if hasattr(obj.Object,'Group'):
+            if hasattr(obj.Object, 'Group'):
                 return obj.Object
-            if hasattr(obj.Object, "Shape") and len(obj.Object.Shape.Vertexes) > 1:
+            elif hasattr(obj.Object, "Shape") and len(obj.Object.Shape.Vertexes) > 1:
                 res.append(obj.Object)
-            if hasattr(obj.Object, "Points"):
+            elif hasattr(obj.Object, "Points"):
                 res.append(obj.Object)
         if res:
             return res
@@ -355,21 +361,24 @@ class approx:
         source = self.parseSel(s)
         if not source:
             return False
-        if not isinstance(source,list):
-            obj=FreeCAD.ActiveDocument.addObject("Part::FeaturePython","Approximation_Surface") #add object to document
-            Approximate(obj,source)
+        if not isinstance(source, list):
+            obj = FreeCAD.ActiveDocument.addObject("Part::FeaturePython", "Approximation_Surface") #add object to document
+            Approximate(obj, source)
             ViewProviderApp(obj.ViewObject)
-            #s.ViewObject.Visibility=False
+            # s.ViewObject.Visibility = False
         else:
             for s in source:
-                obj=FreeCAD.ActiveDocument.addObject("Part::FeaturePython","Approximation_Curve") #add object to document
-                Approximate(obj,s)
+                obj = FreeCAD.ActiveDocument.addObject("Part::FeaturePython", "Approximation_Curve") #add object to document
+                Approximate(obj, s)
                 ViewProviderApp(obj.ViewObject)
-                s.ViewObject.Visibility=False
+                s.ViewObject.Visibility = False
         FreeCAD.ActiveDocument.recompute()
-            
+
     def GetResources(self):
-        return {'Pixmap' : TOOL_ICON, 'MenuText': 'Approximate', 'ToolTip': 'Approximate points to NURBS curve or surface'}
+        return {'Pixmap': TOOL_ICON,
+                'MenuText': 'Approximate',
+                'ToolTip': 'Approximate points to NURBS curve or surface'}
+
 
 FreeCADGui.addCommand('Approximate', approx())
 
