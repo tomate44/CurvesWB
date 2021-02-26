07023c386f10dc79b1b75c60f03d7118ce837250
tomate44 <cg@grellier.fr>
2021-02-26 09:00:59 +0100

    ruled surface fix

--- freecad/Curves/_utils.py
+++ freecad/Curves/_utils.py
@@ -174,13 +174,17 @@ def rootNode(shape, mode=2, deviation=0.3, angle=0.4):
 
 def ruled_surface(e1, e2, normalize=False):
     """creates a ruled surface between 2 edges, with automatic orientation."""
+    fp1 = e1.valueAt(e1.FirstParameter)
+    lp1 = e1.valueAt(e1.LastParameter)
+    fp2 = e2.valueAt(e2.FirstParameter)
+    lp2 = e2.valueAt(e2.LastParameter)
+    line11 = Part.MakeLine(fp1, fp2)
+    line12 = Part.MakeLine(lp1, lp2)
+    line21 = Part.MakeLine(fp1, lp2)
+    line22 = Part.MakeLine(lp1, fp2)
+    if line21.Length + line22.Length < line11.Length + line12.Length:
+        e2.reverse()
     ruled = Part.makeRuledSurface(e1, e2)
-    try:
-        ruled.check(True)
-    except ValueError:
-        e3 = e2.copy()
-        e3.reverse()
-        ruled = Part.makeRuledSurface(e1, e3)
     if normalize:
         s = ruled.Surface
         u0, u1, v0, v1 = s.bounds()
