5caa2c45e7799e2acce351129f7eba7c0d8bf175
tomate44 <cg@grellier.fr>
2021-04-13 18:31:14 +0200

    box_edit WIP

--- freecad/Curves/box_edit.py
+++ freecad/Curves/box_edit.py
@@ -5,25 +5,77 @@ from pivy import coin
 from freecad.Curves import graphics
 
 
-class ConnectionMarker(graphics.Marker):
+# class ConnectionMarker(graphics.Marker):
+    # def __init__(self, points):
+        # super(ConnectionMarker, self).__init__(points, True)
+        # self.output = coin.SoDecomposeVec3f()
+        # self.output.vector.connectFrom(self.data.point)
+        # self.input = coin.SoComposeVec3f()
+        # self.data.point.connectFrom(self.input.vector)
+
+    # def connect_x(self, other):
+        # self.input.x.connectFrom(other.output.x)
+        # other.input.x.connectFrom(self.output.x)
+
+    # def connect_y(self, other):
+        # self.input.y.connectFrom(other.output.y)
+        # other.input.y.connectFrom(self.output.y)
+
+    # def connect_z(self, other):
+        # self.input.z.connectFrom(other.output.z)
+        # other.input.z.connectFrom(self.output.z)
+
+
+class ConstrainedMarker(graphics.Marker):
     def __init__(self, points):
-        super(ConnectionMarker, self).__init__(points, True)
-        self.output = coin.SoDecomposeVec3f()
-        self.output.vector.connectFrom(self.data.point)
-        self.input = coin.SoComposeVec3f()
-        self.data.point.connectFrom(self.input.vector)
+        super(ConstrainedMarker, self).__init__(points, True)
+        self.constraint = (1.0, 1.0, 1.0)
+        self.x_drag_objects = []
+        self.y_drag_objects = []
+        self.z_drag_objects = []
+        self.on_drag_start.append(self.init_constraints)
+        self.on_drag_start.append(self.release)
+        self.on_drag_release.append(self.release_constraints)
 
-    def connect_x(self, other):
-        self.input.x.connectFrom(other.output.x)
-        other.input.x.connectFrom(self.output.x)
+    @property
+    def drag_objects(self):
+        if self.enabled:
+            return [self] + self.x_drag_objects + self.y_drag_objects + self.z_drag_objects
 
-    def connect_y(self, other):
-        self.input.y.connectFrom(other.output.y)
-        other.input.y.connectFrom(self.output.y)
+    def drag(self, mouse_coords, fact=1.):
+        if self.enabled:
+            pts = self.points
+            for i, pt in enumerate(pts):
+                pt[0] = mouse_coords[0] * fact * self.constraint[0] + self._tmp_points[i][0]
+                pt[1] = mouse_coords[1] * fact * self.constraint[1] + self._tmp_points[i][1]
+                pt[2] = mouse_coords[2] * fact * self.constraint[2] + self._tmp_points[i][2]
+            self.points = pts
+            for foo in self.on_drag:
+                foo()
 
-    def connect_z(self, other):
-        self.input.z.connectFrom(other.output.z)
-        other.input.z.connectFrom(self.output.z)
+    def init_constraints(self):
+        for o in self.x_drag_objects:
+            o.x_drag()
+        for o in self.y_drag_objects:
+            o.y_drag()
+        for o in self.z_drag_objects:
+            o.z_drag()
+
+    def release_constraints(self):
+        for o in self.x_drag_objects + self.y_drag_objects + self.z_drag_objects:
+            o.release()
+
+    def x_drag(self):
+        self.constraint = (1.0, 0.0, 0.0)
+
+    def y_drag(self):
+        self.constraint = (0.0, 1.0, 0.0)
+
+    def z_drag(self):
+        self.constraint = (0.0, 0.0, 1.0)
+
+    def release(self):
+        self.constraint = (1.0, 1.0, 1.0)
 
 
 class ConnectionLine(graphics.Line):
@@ -45,6 +97,7 @@ class ConnectionLine(graphics.Line):
         if any([m._delete for m in self.markers]):
             self.delete()
 
+
 class RectangleEditor:
     """Rectangle free-hand editor
     my_editor = RectangleEditor(bounds, fp)"""
@@ -54,16 +107,24 @@ class RectangleEditor:
         self.root_inserted = False
         self.update_func = None
 
+        self.points = [ConstrainedMarker([FreeCAD.Vector(0, 0, 0)]) for i in range(4)]
         u0, u1, v0, v1 = bounds
-        self.points.append(ConnectionMarker([(u0, v0, 0)]))
-        self.points.append(ConnectionMarker([(u0, v1, 0)]))
-        self.points.append(ConnectionMarker([(u1, v1, 0)]))
-        self.points.append(ConnectionMarker([(u1, v0, 0)]))
+        self.points[0].points = [(u0, v0, 0)]
+        self.points[1].points = [(u0, v1, 0)]
+        self.points[2].points = [(u1, v1, 0)]
+        self.points[3].points = [(u1, v0, 0)]
 
-        self.points[1].connect_x(self.points[0])
-        self.points[3].connect_y(self.points[0])
-        self.points[3].connect_x(self.points[2])
-        self.points[1].connect_y(self.points[2])
+        self.points[0].x_drag_objects.append(self.points[1])
+        self.points[0].y_drag_objects.append(self.points[3])
+
+        self.points[1].x_drag_objects.append(self.points[0])
+        self.points[1].y_drag_objects.append(self.points[2])
+
+        self.points[2].x_drag_objects.append(self.points[3])
+        self.points[2].y_drag_objects.append(self.points[1])
+
+        self.points[3].x_drag_objects.append(self.points[2])
+        self.points[3].y_drag_objects.append(self.points[0])
 
         # Setup coin objects
         if self.fp:
@@ -76,6 +137,9 @@ class RectangleEditor:
         self.rm = self.view.getViewer().getSoRenderManager()
         self.sg = self.view.getSceneGraph()
         self.setup_InteractionSeparator()
+
+        print(f"init : {self.bounds()}")
+
         self.update()
 
     def setup_InteractionSeparator(self):
