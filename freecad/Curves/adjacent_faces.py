83fe0d13d802e4ff0f9f4c651dbce9a8e3a919d3
tomate44 <cg@grellier.fr>
2021-05-02 22:13:04 +0200

    adjacent_faces: code cleanup

--- freecad/Curves/adjacent_faces.py
+++ freecad/Curves/adjacent_faces.py
@@ -4,16 +4,18 @@ __title__ = "Select Adjacent faces"
 __author__ = "Christophe Grellier (Chris_G)"
 __license__ = "LGPL 2.1"
 __doc__ = "Select the Adjacent faces of the selected subshape"
+__usage__ = """Select a face or an edge in the 3D View, activate tool
+and all the faces that touch it will be added to the selection."""
 
-# import FreeCAD
+import os
+import FreeCAD
 from FreeCADGui import Selection as sel
 from FreeCADGui import addCommand
 import Part
-from freecad.Curves import _utils
+from . import ICONPATH
 
-TOOL_ICON = 'WhatsThis.svg'
-# debug = _utils.debug
-debug = _utils.doNothing
+
+TOOL_ICON = os.path.join(ICONPATH, 'adjacent_faces.svg')
 
 
 class adjacentfacesCommand:
@@ -23,15 +25,15 @@ class adjacentfacesCommand:
         if isinstance(sub, Part.Face):
             for i in range(len(shape.Faces)):
                 if sub.isEqual(shape.Faces[i]):
-                    return "Face{}".format(i+1)
+                    return "Face{}".format(i + 1)
         elif isinstance(sub, Part.Edge):
             for i in range(len(shape.Edges)):
                 if sub.isEqual(shape.Edges[i]):
-                    return "Edge{}".format(i+1)
+                    return "Edge{}".format(i + 1)
         if isinstance(sub, Part.Vertex):
             for i in range(len(shape.Vertexes)):
                 if sub.isEqual(shape.Vertexes[i]):
-                    return "Vertex{}".format(i+1)
+                    return "Vertex{}".format(i + 1)
 
     def Activated(self):
         search_type = Part.Face
@@ -55,18 +57,19 @@ class adjacentfacesCommand:
                     result += anc
                     for a in anc:
                         sel.addSelection(obj, self.get_subname(shape, a))
+        if len(result) == 0:
+            FreeCAD.Console.PrintError("{} :\n{}\n".format(__title__, __usage__))
 
     def IsActive(self):
-        s = sel.getSelectionEx()
-        for selo in s:
-            if selo.HasSubObjects:
-                return(True)
-        return(False)
+        if FreeCAD.ActiveDocument:
+            return True
+        else:
+            return False
 
     def GetResources(self):
         return {'Pixmap': TOOL_ICON,
                 'MenuText': 'Adjacent faces',
-                'ToolTip': 'Select the adjacent faces of the selected shape'}
+                'ToolTip': "{}\n{}".format(__doc__, __usage__)}
 
 
-addCommand('adjacent_faces', adjacentfacesCommand())
+addCommand('Curves_adjacent_faces', adjacentfacesCommand())
