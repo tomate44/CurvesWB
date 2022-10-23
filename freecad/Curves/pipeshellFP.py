df0ccd716c3f1c7fd3981af4ce2267c25bf02741
tomate44 <cg@grellier.fr>
2022-10-23 22:17:24 +0200

    PipeShell: allow multiple wires in profile

--- freecad/Curves/pipeshellFP.py
+++ freecad/Curves/pipeshellFP.py
@@ -12,76 +12,79 @@ import Part
 from freecad.Curves import _utils
 from freecad.Curves import ICONPATH
 
-TOOL_ICON = os.path.join( ICONPATH, 'pipeshell.svg')
+TOOL_ICON = os.path.join(ICONPATH, 'pipeshell.svg')
 DEBUG = False
+
 
 def debug(string):
     if DEBUG:
         FreeCAD.Console.PrintMessage(string)
         FreeCAD.Console.PrintMessage("\n")
 
+
 class pipeShell:
     "PipeShell featurePython object"
+
     def __init__(self, obj):
         ''' Add the properties '''
-        obj.addProperty("App::PropertyLinkSubList","Spine",       "Main", "Sweep path")
-        obj.addProperty("App::PropertyLinkList",   "Profiles",    "Main", "Profiles that are swept along spine")
-        obj.addProperty("App::PropertyLink",       "Support",     "Mode", "Shape of the ShapeSupport mode")
-        obj.addProperty("App::PropertyLink",       "Auxiliary",   "Mode", "Auxiliary spine")
-        obj.addProperty("App::PropertyEnumeration","Mode",        "Main", "PipeShell mode").Mode = ["Frenet","DiscreteTrihedron","FixedTrihedron","Binormal","ShapeSupport","AuxiliarySpine"]
-        obj.addProperty("App::PropertyEnumeration","Output",      "Main", "Output shape").Output = ["Sections","Lofted sections","Surface"]
-        obj.addProperty("App::PropertyBool",       "Solid",       "Settings",  "Make solid object").Solid = False
-        obj.addProperty("App::PropertyInteger",    "MaxDegree",   "Settings",  "Maximum degree of the generated surface").MaxDegree = 5
-        obj.addProperty("App::PropertyInteger",    "MaxSegments", "Settings",  "Maximum number of segments of the generated surface").MaxSegments = 999
-        obj.addProperty("App::PropertyInteger",    "Samples",     "Settings",  "Number of samples for preview").Samples = 100
-        obj.addProperty("App::PropertyFloat",      "Tol3d",       "Settings",  "Tolerance 3D").Tol3d = 1.0e-4
-        obj.addProperty("App::PropertyFloat",      "TolBound",    "Settings",  "Tolerance boundary").TolBound = 1.0e-4
-        obj.addProperty("App::PropertyFloat",      "TolAng",      "Settings",  "Tolerance angular").TolAng = 1.0e-2
-        obj.addProperty("App::PropertyVector",     "Direction",   "Mode",      "Direction of the Binormal and FixedTrihedron modes")
-        obj.addProperty("App::PropertyVector",     "Location",    "Mode",      "Location of the FixedTrihedron mode")
-        obj.addProperty("App::PropertyBool",       "Corrected",   "Mode",      "Corrected Frenet").Corrected = False
-        obj.addProperty("App::PropertyBool",       "EquiCurvi",   "Mode",      "Curvilinear equivalence").EquiCurvi = False
-        obj.addProperty("App::PropertyEnumeration","Contact",     "Mode",      "Type of contact to auxiliary spine").Contact = ["NoContact","Contact","ContactOnBorder"]
+        obj.addProperty("App::PropertyLinkSubList", "Spine", "Main", "Sweep path")
+        obj.addProperty("App::PropertyLinkList", "Profiles", "Main", "Profiles that are swept along spine")
+        obj.addProperty("App::PropertyLink", "Support", "Mode", "Shape of the ShapeSupport mode")
+        obj.addProperty("App::PropertyLink", "Auxiliary", "Mode", "Auxiliary spine")
+        obj.addProperty("App::PropertyEnumeration", "Mode", "Main", "PipeShell mode").Mode = ["Frenet", "DiscreteTrihedron", "FixedTrihedron", "Binormal", "ShapeSupport", "AuxiliarySpine"]
+        obj.addProperty("App::PropertyEnumeration", "Output", "Main", "Output shape").Output = ["Sections", "Lofted sections", "Surface"]
+        obj.addProperty("App::PropertyBool", "Solid", "Settings", "Make solid object").Solid = False
+        obj.addProperty("App::PropertyInteger", "MaxDegree", "Settings", "Maximum degree of the generated surface").MaxDegree = 5
+        obj.addProperty("App::PropertyInteger", "MaxSegments", "Settings", "Maximum number of segments of the generated surface").MaxSegments = 999
+        obj.addProperty("App::PropertyInteger", "Samples", "Settings", "Number of samples for preview").Samples = 100
+        obj.addProperty("App::PropertyFloat", "Tol3d", "Settings", "Tolerance 3D").Tol3d = 1.0e-4
+        obj.addProperty("App::PropertyFloat", "TolBound", "Settings", "Tolerance boundary").TolBound = 1.0e-4
+        obj.addProperty("App::PropertyFloat", "TolAng", "Settings", "Tolerance angular").TolAng = 1.0e-2
+        obj.addProperty("App::PropertyVector", "Direction", "Mode", "Direction of the Binormal and FixedTrihedron modes")
+        obj.addProperty("App::PropertyVector", "Location", "Mode", "Location of the FixedTrihedron mode")
+        obj.addProperty("App::PropertyBool", "Corrected", "Mode", "Corrected Frenet").Corrected = False
+        obj.addProperty("App::PropertyBool", "EquiCurvi", "Mode", "Curvilinear equivalence").EquiCurvi = False
+        obj.addProperty("App::PropertyEnumeration", "Contact", "Mode", "Type of contact to auxiliary spine").Contact = ["NoContact", "Contact", "ContactOnBorder"]
         obj.Mode = "DiscreteTrihedron"
         obj.Contact = "NoContact"
         obj.Output = "Sections"
-        obj.Direction = FreeCAD.Vector(0,0,1)
-        obj.Location = FreeCAD.Vector(0,0,0)
+        obj.Direction = FreeCAD.Vector(0, 0, 1)
+        obj.Location = FreeCAD.Vector(0, 0, 0)
         obj.Proxy = self
 
     def getprop(self, obj, prop):
         if hasattr(obj, prop):
-            return(obj.getPropertyByName(prop))
+            return obj.getPropertyByName(prop)
         else:
-            FreeCAD.Console.PrintError("\n%s object has no property %s\n"%(obj.Label, prop))
-            return(None)
+            FreeCAD.Console.PrintError("\n%s object has no property %s\n" % (obj.Label, prop))
+            return None
 
     def getWires(self, obj, prop):
         res = []
-        content = self.getprop( obj, prop)
-        if isinstance(content,(list,tuple)):
-            for l in content:
-                res.append(l.Shape.Wires[0])
-            return(res)
+        content = self.getprop(obj, prop)
+        if isinstance(content, (list, tuple)):
+            for li in content:
+                res.append(li.Shape.Wires[0])
+            return res
         else:
             if content.Shape.Wires:
-                return(content.Shape.Wires[0])
+                return content.Shape.Wires[0]
             elif content.Shape.Edges:
-                return(Part.Wire([content.Shape.Edges[0]]))
+                return Part.Wire([content.Shape.Edges[0]])
 
     def getVertex(self, obj, prop):
         res = None
-        content = self.getprop( obj, prop)
+        content = self.getprop(obj, prop)
         if not content:
-            return(res)
+            return res
         o = content[0]
         for ss in content[1]:
             n = eval(ss.lstrip('Vertex'))
-            res = o.Shape.Vertexes[n-1]
-        return(res)
+            res = o.Shape.Vertexes[n - 1]
+        return res
 
     def onChanged(self, fp, prop):
-        debug("%s changed"%prop)
+        debug("%s changed" % prop)
         if prop == "Mode":
             if fp.Mode == "Frenet":
                 fp.setEditorMode("Corrected", 0)
@@ -167,30 +170,29 @@ class pipeShell:
                 fp.Contact = "Contact"
 
     def add(self, ps, p):
-        contact = self.getprop( p, "Contact")
-        correction = self.getprop( p, "Correction")
-        loc = self.getVertex(p,"Location")
-        if p.Shape.Wires:
-            shape = p.Shape.Wires[0]
+        contact = self.getprop(p, "Contact")
+        correction = self.getprop(p, "Correction")
+        loc = self.getVertex(p, "Location")
+        for shape in p.Shape.Wires:
             if loc:
-                debug("Adding Profile %s at location %s"%(p.Label,loc.Point))
+                debug("Adding Profile %s at location %s" % (p.Label, loc.Point))
                 ps.add(shape, loc, contact, correction)
             else:
-                debug("Adding Profile %s"%p.Label)
+                debug("Adding Profile %s" % p.Label)
                 ps.add(shape, contact, correction)
 
     def execute(self, obj):
         debug("\n\nExecuting PipeShell\n")
         path = None
         profs = []
-        edges =  _utils.getShape(obj, "Spine", "Edge")
+        edges = _utils.getShape(obj, "Spine", "Edge")
         path = Part.Wire(Part.__sortEdges__(edges))
         if path.isValid():
-            debug("Valid spine : %s"%path)
+            debug("Valid spine : %s" % path)
         if hasattr(obj, "Profiles"):
             profs = obj.Profiles
         if not (path and profs):
-            return(None)
+            return None
         debug("Creating PipeShell")
         # create the pipeShell object
         ps = Part.BRepOffsetAPI.MakePipeShell(path)
@@ -200,28 +202,28 @@ class pipeShell:
         tb = self.getprop(obj, "TolBound") or 1.0e-4
         ta = self.getprop(obj, "TolAng") or 1.0e-2
         ps.setTolerance(t3, tb, ta)
-        
-        mode = self.getprop(obj, "Mode")# or "DiscreteTrihedron"
-        if mode in ["Binormal","FixedTrihedron"]:
+
+        mode = self.getprop(obj, "Mode")  # or "DiscreteTrihedron"
+        if mode in ["Binormal", "FixedTrihedron"]:
             direction = self.getprop(obj, "Direction")
             if not direction:
-                direction = FreeCAD.Vector(0,0,1)
+                direction = FreeCAD.Vector(0, 0, 1)
                 obj.Direction = direction
                 FreeCAD.Console.PrintError("\nWrong direction, defaulting to +Z\n")
             elif direction.Length < 1e-7:
-                direction = FreeCAD.Vector(0,0,1)
+                direction = FreeCAD.Vector(0, 0, 1)
                 obj.Direction = direction
                 FreeCAD.Console.PrintError("\nDirection has null length, defaulting to +Z\n")
             if mode == "Binormal":
-                debug("Binormal mode (%r)"%direction)
+                debug("Binormal mode (%r)" % direction)
                 ps.setBiNormalMode(direction)
             elif mode == "FixedTrihedron":
-                loc = self.getprop(obj, "Location") or FreeCAD.Vector(0,0,0)
-                debug("FixedTrihedron mode (%r %r)"%(loc, direction))
+                loc = self.getprop(obj, "Location") or FreeCAD.Vector(0, 0, 0)
+                debug("FixedTrihedron mode (%r %r)" % (loc, direction))
                 ps.setTrihedronMode(loc, direction)
         elif mode == "Frenet":
             fre = self.getprop(obj, "Corrected")
-            debug("Frenet mode (%r)"%fre)
+            debug("Frenet mode (%r)" % fre)
             ps.setFrenetMode(fre)
         elif mode == "AuxiliarySpine":
             aux = self.getprop(obj, "Auxiliary")
@@ -235,8 +237,8 @@ class pipeShell:
                 curv = self.getprop(obj, "EquiCurvi") or False
                 cont = self.getprop(obj, "Contact") or "NoContact"
                 n = self.getCode(cont)
-                debug("AuxiliarySpine mode (%r %s)"%(curv,cont))
-                ps.setAuxiliarySpine(w,curv,n)
+                debug("AuxiliarySpine mode (%r %s)" % (curv, cont))
+                ps.setAuxiliarySpine(w, curv, n)
             else:
                 FreeCAD.Console.PrintError("\nPlease set a valid Auxiliary Spine Object\n")
         elif mode == "ShapeSupport":
@@ -250,14 +252,13 @@ class pipeShell:
             else:
                 FreeCAD.Console.PrintError("\nPlease set a valid Spine support Object\n")
 
-
         for p in profs:
-            self.add(ps,p)
-        
+            self.add(ps, p)
+
         if ps.isReady():
             output = self.getprop(obj, "Output")
             solid = self.getprop(obj, "Solid") or False
-            if (output == "Surface") or (not hasattr(ps,'simulate')):
+            if (output == "Surface") or (not hasattr(ps, 'simulate')):
                 ps.build()
                 if solid:
                     ps.makeSolid()
@@ -275,11 +276,11 @@ class pipeShell:
 
     def getCode(self, cont):
         if cont == "Contact":
-            return(1)
+            return 1
         elif cont == "ContactOnBorder":
-            return(2)
+            return 2
         else:
-            return(0)
+            return 0
 
     def getRails(self, shapes):
         nbvert = len(shapes[0].Vertexes)
@@ -292,59 +293,58 @@ class pipeShell:
                 bs = Part.BSplineCurve()
                 bs.interpolate(pts)
                 edges.append(bs.toShape())
-                debug("Rail %d : BSpline curve"%i)
+                debug("Rail %d : BSpline curve" % i)
             except Part.OCCError:
                 po = Part.makePolygon(pts)
                 edges.append(po)
-                debug("Rail %d : Polygon"%i)
-        return(edges)
-                
+                debug("Rail %d : Polygon" % i)
+        return edges
+
 
 class pipeShellVP:
-    def __init__(self,vobj):
+    def __init__(self, vobj):
         vobj.Proxy = self
-       
+
     def getIcon(self):
         return TOOL_ICON
 
     def attach(self, vobj):
         self.ViewObject = vobj
         self.Object = vobj.Object
-  
-    def setEdit(self,vobj,mode):
+
+    def setEdit(self, vobj, mode):
         return False
-    
-    def unsetEdit(self,vobj,mode):
+
+    def unsetEdit(self, vobj, mode):
         return
 
     def __getstate__(self):
         return None
 
-    def __setstate__(self,state):
+    def __setstate__(self, state):
         return None
 
     def claimChildren(self):
         return (self.Object.Profiles + [self.Object.Spine])
-        
-    def onDelete(self, feature, subelements): # subelements is a tuple of strings
+
+    def onDelete(self, feature, subelements):  # subelements is a tuple of strings
         return True
 
 
 class pipeShellCommand:
     "creates a PipeShell feature python object"
-    def makePipeShellFeature(self,path,profs):
+
+    def makePipeShellFeature(self, path, profs):
         if path and profs:
-            psfp = FreeCAD.ActiveDocument.addObject("Part::FeaturePython","PipeShell")
+            psfp = FreeCAD.ActiveDocument.addObject("Part::FeaturePython", "PipeShell")
             pipeShell(psfp)
             psfp.Spine = path
             psfp.Profiles = profs
             pipeShellVP(psfp.ViewObject)
-            #psfp.ViewObject.LineWidth = 2.0
-            #psfp.ViewObject.LineColor = (0.5,0.8,0.3)
+            # psfp.ViewObject.LineWidth = 2.0
+            # psfp.ViewObject.LineColor = (0.5,0.8,0.3)
             psfp.Mode = "DiscreteTrihedron"
             FreeCAD.ActiveDocument.recompute()
-            
-        
 
     def Activated(self):
         path = list()
@@ -353,29 +353,30 @@ class pipeShellCommand:
         for selobj in sel:
             if selobj.HasSubObjects:
                 for sub in selobj.SubElementNames:
-                    FreeCAD.Console.PrintMessage(sub+"\n")
+                    FreeCAD.Console.PrintMessage(sub + "\n")
                     if sub[0:4] == 'Edge':
-                        path.append((selobj.Object,(sub,)))
-            elif hasattr(selobj.Object,'Proxy'):
+                        path.append((selobj.Object, (sub, )))
+            elif hasattr(selobj.Object, 'Proxy'):
                 if selobj.Object.Proxy.__module__ == 'freecad.Curves.pipeshellProfileFP':
                     profs.append(selobj.Object)
-        FreeCAD.Console.PrintMessage(str(path)+"\n")
-        FreeCAD.Console.PrintMessage(str(profs)+"\n")
+        FreeCAD.Console.PrintMessage(str(path) + "\n")
+        FreeCAD.Console.PrintMessage(str(profs) + "\n")
         if path and profs:
-            #path.ViewObject.LineColor = (1.0,0.3,0.0)
-            self.makePipeShellFeature(path,profs)
+            # path.ViewObject.LineColor = (1.0,0.3,0.0)
+            self.makePipeShellFeature(path, profs)
         else:
             FreeCAD.Console.PrintError("\nYou must select:\n- in the 3D view, the edges that build the sweep path\n- in the Tree view, one or more 'pipeshellProfile' objects\n")
-        
+
     def IsActive(self):
         if FreeCAD.ActiveDocument:
-            return(True)
+            return True
         else:
-            return(False)
+            return False
 
     def GetResources(self):
         return {'Pixmap': TOOL_ICON,
                 'MenuText': __title__,
                 'ToolTip': __doc__}
+
 
 FreeCADGui.addCommand('pipeshell', pipeShellCommand())
