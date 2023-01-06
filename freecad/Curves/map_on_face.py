30a279808e48808b1a21c5be4dbffb1cc3cee90a
tomate44 <cg@grellier.fr>
2023-01-06 17:39:22 +0100

    Map on face (4)

--- freecad/Curves/map_on_face.py
+++ freecad/Curves/map_on_face.py
@@ -27,6 +27,83 @@ def stretched_plane(poles, param_range=[0, 2, 0, 2], extend_factor=1.0):
     return bs
 
 
+class Quad:
+    def __init__(self, bounds=None):
+        self.quad = Part.BSplineSurface()
+        if bounds is not None:
+            self.setPoles(*bounds)
+
+    @property
+    def Limits(self):
+        pu1, pu2 = self.quad.getPoles()
+        p00 = pu1[0]
+        p11 = pu2[-1]
+        return p00.x, p11.x, p00.y, p11.y
+
+    @Limits.setter
+    def Limits(self, bounds):
+        if not (len(bounds) == 4):
+            raise RuntimeError("Quad need 4 bounds")
+        u0, u1, v0, v1 = bounds
+        self.quad.setPole(1, 1, FreeCAD.Vector(u0, v0, 0))
+        self.quad.setPole(1, 2, FreeCAD.Vector(u0, v1, 0))
+        self.quad.setPole(2, 1, FreeCAD.Vector(u1, v0, 0))
+        self.quad.setPole(2, 2, FreeCAD.Vector(u1, v1, 0))
+
+    @property
+    def Bounds(self):
+        return self.quad.bounds()
+
+    @Bounds.setter
+    def Bounds(self, bounds):
+        if not (len(bounds) == 4):
+            raise RuntimeError("Quad need 4 bounds")
+        self.quad.setUKnot(1, bounds[0])
+        self.quad.setUKnot(2, bounds[1])
+        self.quad.setVKnot(1, bounds[2])
+        self.quad.setVKnot(2, bounds[3])
+        # self.quad.scaleKnotsToBounds(*bounds) is less precise
+
+    def extend(self, *bounds):
+        if not (len(bounds) == 4):
+            raise RuntimeError("Quad need 4 bounds")
+        u0, u1, v0, v1 = self.Limits
+        s0, s1, t0, t1 = bounds
+        ku0, ku1, kv0, kv1 = self.Bounds
+        nu0, nu1, nv0, nv1 = self.Bounds
+        if s0 < u0:
+            nu0 += (ku1 - ku0) * (s0 - u0) / (u1 - u0)
+        if s1 > u1:
+            nu1 += (ku1 - ku0) * (s1 - u1) / (u1 - u0)
+        if t0 < v0:
+            nv0 += (kv1 - kv0) * (t0 - v0) / (v1 - v0)
+        if t1 > v1:
+            nv1 += (kv1 - kv0) * (t1 - v1) / (v1 - v0)
+        self.Limits = bounds
+        self.Bounds = nu0, nu1, nv0, nv1
+
+    def reverseU(self):
+        u1, u2 = self.quad.getPoles()
+        self.quad.setPole(1, 1, u2[0])
+        self.quad.setPole(1, 2, u2[1])
+        self.quad.setPole(2, 1, u1[0])
+        self.quad.setPole(2, 2, u1[1])
+
+    def reverseV(self):
+        u1, u2 = self.quad.getPoles()
+        self.quad.setPole(1, 1, u1[1])
+        self.quad.setPole(1, 2, u1[0])
+        self.quad.setPole(2, 1, u2[1])
+        self.quad.setPole(2, 2, u2[0])
+
+    def swapUV(self):
+        u1, u2 = self.quad.getPoles()
+        self.quad.setPole(1, 1, u2[1])
+        self.quad.setPole(1, 2, u2[0])
+        self.quad.setPole(2, 1, u1[1])
+        self.quad.setPole(2, 2, u1[0])
+
+
 class MapOnFace:
     """Map a shape on a target face
     """
@@ -85,7 +162,17 @@ class MapOnFace:
         return u0, u1, v0, v1
 
 
+"""
+reload(map_on_face)
+q = map_on_face.Quad()
+q.Limits = [-2, 5, -1, 10]
+q.Limits
+q.Bounds
+q.Bounds = [-1, 2, -3, 4]
+q.Bounds
+q.extend(-5, 7, -2, 12)
+q.Limits
+q.Bounds
+q.quad.parameter(FreeCAD.Vector(-2, -1, 0))
 
-
-
-
+"""
