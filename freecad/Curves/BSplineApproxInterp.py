24cd235fe761a6d39f0d7b78b27a929d807250dc
tomate44 <cg@grellier.fr>
2025-03-18 12:05:11 +0100

    0.6.60 License fixes

--- freecad/Curves/BSplineApproxInterp.py
+++ freecad/Curves/BSplineApproxInterp.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: 2018 German Aerospace Center (DLR)
+
 #  This file is a python port of the following files :
 #
 #  /src/geometry/CTiglBSplineApproxInterp.cpp
